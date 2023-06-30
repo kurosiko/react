f5c8207e21d79bad63db13367afb1df21b3d7b4d
kurosiko <ttnntt1234567890987654321@gmail.com>
2023-07-01 07:29:15 +0900

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Tab,Tabs,TabList,TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,14 +17,24 @@ function App() {
           <a href='https://twitter.com/kurosiko'>Twitter</a>
         </div>
         <div id='space'>
-          <h2>This is test page for react!</h2>
-          <p>
-            Original Page ↓<br></br>
-            <a href='https://kurosiko.github.io/'>here</a>
-          </p>
-          <button onClick={()=>setCount((count)=>count+1)}>
-            count is {count}
-          </button>
+          <Tabs>
+            <TabList>
+              <Tab>
+                Ex.
+              </Tab>
+            </TabList>
+
+            <TabPanel>
+              <h2>This is test page for react!</h2>
+              <p>Original Page ↓
+                <br></br>
+                <a href='https://kurosiko.github.io/'>here</a>
+              </p>
+              <button onClick={()=>setCount((count)=>count+1)}>
+              count is {count}
+              </button>
+            </TabPanel>
+          </Tabs>
         </div>
       </div>
     </>
